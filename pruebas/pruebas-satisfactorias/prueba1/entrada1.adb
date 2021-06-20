4162161256e03ed42dfafc0548b41ef8895468ea
Jorge Moreno <jorge.velez.moreno@gmail.com>
2021-06-20 21:23:42 +0200

    Bateria de pruebas
    Bateria de pruebas correctas y erroneas realizadas.

---
+++ pruebas/pruebas-satisfactorias/prueba1/entrada1.adb
@@ -0,0 +1,10 @@
+procedure Main is
+
+   variable_string1 : String := "String 1";
+   variable_int1 : Integer := 2 + 10 ;
+   variable_int2 : Integer := 3 + 7 ;
+   variable_float1 : Float := 1.8;
+   variable_bool : Boolean :=  2.9 == variable_float1 ;
+   variable_ops1 : Boolean := variable_int1 > variable_int2 ;
+
+end Main ;
---
+++ pruebas/pruebas-satisfactorias/prueba2/entrada2.adb
@@ -0,0 +1,14 @@
+procedure Main is
+
+   variable_int1 : Integer := 2 + 10 ;
+   variable_int2 : Integer := 3 + 7 ;
+   variable_float1 : Float := 2.9;
+   variable_bool : Boolean :=  2.9 == variable_float1 ;
+
+
+   if variable_float1 > 0.0 and variable_bool then
+     variable_int1 := variable_int1 * 2;
+     else
+      variable_float1 := variable_int1 / 2;
+   end if;
+end Main ;
